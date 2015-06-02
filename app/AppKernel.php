42e22a7eb5743d7abb823071d2261a0726f57f3a
Tom Ellis <tellishtc@gmail.com>
2015-06-02 12:02:34 +0100

    Update AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -12,6 +12,7 @@ class AppKernel extends Kernel
             new \Tomahawk\Bundle\MigrationsBundle\MigrationsBundle(),
             new \Tomahawk\Bundle\DoctrineBundle\DoctrineBundle(),
             new \Tomahawk\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new \Tomahawk\Bundle\CSRFBundle\CSRFBundle(),
             new \MyPackage\Mybundle()
         );
 
@@ -25,9 +26,17 @@ class AppKernel extends Kernel
     public function registerMiddleware()
     {
         $middlewares = array(
+            // Converts string responses from controllers into responses
             new \Tomahawk\HttpCore\Middleware\StringResponse(),
+
+            // Add newly created cookies to headers
             new \Tomahawk\HttpCore\Middleware\HeaderCookies(),
-            new \Tomahawk\Session\Middleware\Session()
+
+            // Saves the session at the end of the request
+            new \Tomahawk\Session\Middleware\Session(),
+
+            // Logs errors and outputs 404 and 50x error templates
+            new \Tomahawk\HttpKernel\Middleware\ExceptionMiddleware(),
         );
 
         return $middlewares;
